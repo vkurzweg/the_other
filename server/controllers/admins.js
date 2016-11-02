14b9b5240ee5fe298efc1a56d7b7aca666b00855
Victoria Dunham <vkurzweg@gmail.com>
2016-11-01 17:56:44 -0700

    add admin controller

--- server/controllers/admins.js
+++ server/controllers/admins.js
@@ -1,2 +1,46 @@
 var Admin = require("../models/admin");
 
+module.exports = {
+  create: create,
+  me:     me
+};
+
+function create(req, res, next) {
+  if (!req.body.password) {
+    return res.status(422).send('Missing required fields');
+  }
+  Admin
+    .create(req.body)
+    .then(function(user) {
+      res.json({
+        success: true,
+        message: 'Successfully created user.',
+        data: {
+          email: user.email,
+          id:    user._id
+        }
+      });
+    }).catch(function(err) {
+      if (err.message.match(/E11000/)) {
+        err.status = 409;
+      } else {
+        err.status = 422;
+      }
+      next(err);
+    });
+};
+
+// function me(req, res, next) {
+//   Admin
+//     .findOne({email: req.decoded.email}).exec()
+//     .then(function(user) {
+//       res.json({
+//         success: true,
+//         message: 'Successfully retrieved user data.',
+//         data: user
+//       });
+//     })
+//     .catch(function(err) {
+//       next(err);
+//     });
+// };
